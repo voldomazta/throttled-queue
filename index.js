a7d3fd78b062921b4c0a5ae7a02d069294d92632
Ramon Alivio <r@ramon.ph>
2021-03-09 11:09:39 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class PromiseWatcher {
+    constructor(input, transformer) {
+        this._resolved = false;
+        this._input = input;
+        this._promise = new Promise(async (resolve, reject) => {
+            transformer(input, resolve, reject);
+        }).then(o => {
+            this._resolved = true;
+            this._output = o;
+        });
+    }
+    promise() {
+        return this._promise;
+    }
+    resolved() {
+        return this._resolved;
+    }
+    input() {
+        return this._input;
+    }
+    output() {
+        return this._output;
+    }
+}
+async function ThrottledQueue(items, transformer, concurrency = 10) {
+    let queue = [], results = [];
+    while (items.length || queue.length) {
+        if (items.length) {
+            if (queue.length < concurrency) {
+                queue.push(new PromiseWatcher(items.splice(0, 1)[0], transformer));
+            }
+        }
+        if (queue.length == concurrency || !items.length || !items.length && queue.length) {
+            let promises = [];
+            for (let i = 0; i < queue.length; i++) {
+                promises.push(queue[i].promise());
+            }
+            await Promise.race(promises).then(() => {
+                for (let i = 0; i < queue.length; i++) {
+                    if (queue[i].resolved()) {
+                        let promise = queue.splice(i, 1)[0];
+                        results.push({
+                            "input": promise.input(),
+                            "output": promise.output()
+                        });
+                        promise = null;
+                    }
+                }
+            });
+        }
+    }
+    return results;
+}
+exports.default = ThrottledQueue;
