40f9919f44b0baef279a26846a49b83ed43fe177
Ramon Alivio Jr <ramon.alivio@evolutionfinance.com>
2021-03-09 11:30:56 +0800

    restored comments

--- index.js
+++ index.js
@@ -26,18 +26,31 @@ class Resolver {
 }
 async function ThrottledQueue(items, transformer, concurrency = 10) {
     let queue = [], results = [];
+    // Loop until there are no more items to process
     while (items.length || queue.length) {
+        // If we still have items to enqueue
         if (items.length) {
+            // Limit numer of concurrent promises
             if (queue.length < concurrency) {
+                // Take note that promises are created/started at this point
                 queue.push(new Resolver(items.splice(0, 1)[0], transformer));
             }
         }
+        /**
+         * Observe promise resolution when:
+         * 1.) The queue has hit concurrency limit; or
+         * 2.) If we have enqueued everything; or
+         * 3.) #2 and there are still items on the queue
+         */
         if (queue.length == concurrency || !items.length || !items.length && queue.length) {
+            // Get promises from the queue objects
             let promises = [];
             for (let i = 0; i < queue.length; i++) {
                 promises.push(queue[i].promise());
             }
+            // Block until one of the promises have resolved
             await Promise.race(promises).then(() => {
+                // Remove all the resolved promises from the queue, not just this one
                 for (let i = 0; i < queue.length; i++) {
                     if (queue[i].resolved()) {
                         let promise = queue.splice(i, 1)[0];
