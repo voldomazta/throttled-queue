09b27c12811575890d30e5f6c491df367b651d65
Ramon Alivio Jr <ramon.alivio@evolutionfinance.com>
2021-03-14 06:16:20 +0800

    allow resolution without value

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export declare type Settler = (val: any) => void;
+export declare type Settler = (val?: any) => void;
 declare type Executor = (item: any, resolve: Settler, reject: Settler) => void;
 export default function tq(items: any[], executor: Executor, concurrency?: number): Promise<any[]>;
 export {};
